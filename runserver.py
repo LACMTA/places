4dac755d512e8c0e79c0fc22844fb3509bdad36d
Douglas Goodwin <dgoodwin@gmail.com>
2014-12-04 16:21:46 -0800

    old places code

--- runserver.py
+++
@@ -1,9 +0,0 @@
-from os import rename
-import shutil
-from places import app, auth
-from places.models import Place, Department, Product, Service
-
-        
-if __name__ == "__main__":
-    app.run()
-
