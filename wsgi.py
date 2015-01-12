66c7a9ad718f7e0215f871a6c558aa81718b7464
Douglas Goodwin <dgoodwin@gmail.com>
2015-01-12 11:16:15 -0800

    production config

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import os, sys
 sys.path.append('/var/www/envs/places')
 
 from app import create_app
-application = create_app('development')
+application = create_app('production')
 
 # Start server  ===============================================================
 if __name__ == '__main__':		
